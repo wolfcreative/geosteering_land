1f58248acc4053464683bb19efdfe544f3daa1c9
Hidden Nonsense <loonysnow@gmail.com>
2019-04-11 01:48:26 +0500

    update

--- js/all.js
+++ js/all.js
@@ -1,3 +1,14 @@
 $(function() {
-  
+    /* Проверка высоты для ограничения блока */
+    /* START */
+    $( ".reviews .uk-card" ).each(function() {
+        if ($(this).children('.uk-card-body').height() >= 425){
+            $(this).children('.uk-card-body').addClass('active');
+        }
+    });
+
+    $( ".reviews .uk-card-body.active" ).click(function() {
+        $(this).removeClass('active');
+    });
+    /* END */
 });
